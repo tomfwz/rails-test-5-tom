b1e2102c2f2479169002047d4eb99d7f0442aa40
Tom Futureworkz <tom@futureworkz.com>
2015-12-19 13:25:28 +0700

    Update db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,16 @@
 Student.destroy_all
 School.destroy_all
+SchoolMate.destroy_all
 
-Student.seed
-School.seed
+
+us = School.create({ name: 'University of Science', address: '227 Nguyen Van Cu', category: :university })
+lhp = School.create({ name: 'La Hong Phat', address: '225 Nguyen Van Cu', category: :high_school })
+
+tom   = Student.create({ name: 'Tom Fwz', dob: '12 Feb 1993' })
+james = Student.create({ name: 'James Fwz', dob: '31 Dec 1990' })
+robin = Student.create({ name: 'Robin Fwz', dob: '31 Jan 1990' })
+
+SchoolMate.create({ school: us, student: tom })
+SchoolMate.create({ school: us, student: james })
+SchoolMate.create({ school: us, student: robin })
+SchoolMate.create({ school: lhp, student: tom })
