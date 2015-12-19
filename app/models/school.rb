663e912ea0af189bc4dfece26c9e47074c0cbe20
Tom Futureworkz <tom@futureworkz.com>
2015-12-19 09:58:57 +0700

    Update school model to use hay_many though association

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,7 +1,8 @@
 class School < ActiveRecord::Base
   extend Enumerize
   
-  has_many :students
-  
+  has_many :school_mates
+  has_many :students, through: :school_mates
+
   enumerize :type, in: [:university, :high_school, :secondary_school, :primary_school], default: :university, predicates: true
 end
