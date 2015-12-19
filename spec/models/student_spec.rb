40d52735c437673f4ce3025fbd97cab6bac0514e
Tom Futureworkz <tom@futureworkz.com>
2015-12-19 10:09:03 +0700

    Add Student's spec

---
+++ spec/models/student_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe Student do
+  context 'associations' do 
+    it { is_expected.to have_many :schools }
+  end
+
+  context 'validations' do
+    it { is_expected.to validate_presence_of :name }
+    it { is_expected.to validate_presence_of :dob }
+  end
+end
